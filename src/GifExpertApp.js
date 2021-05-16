044da7adff3684dfdfe185c73dd71330288210bf
Francisco Csik <francsik23@gmail.com>
2021-05-15 22:43:55 -0300

    app terminada

---
+++ src/GifExpertApp.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import AddCategory from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+const GiftExpertApp = () => {
+
+    const [categories, setCategories] = useState( ['Star Wars'] );
+
+
+    return(
+        <>
+            <h2>GiftExpertApp</h2>
+            <AddCategory setCategories={ setCategories }/>
+
+            <ol>
+                { categories.map( category => ( 
+                    <GifGrid 
+                        key={ category }
+                        category={ category }
+                    />
+                )) 
+                }
+            </ol>
+
+        </>
+    );
+};
+
+export default GiftExpertApp
